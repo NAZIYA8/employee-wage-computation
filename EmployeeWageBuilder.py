f100e28466eddb277bb9a536d805614c0c137056
NAZIYA8 <syeda.naziya896@gmail.com>
2021-07-08 21:20:32 +0530

    [Naziya]Add: Welcome to Employee Wage Computation Problem

---
+++ EmployeeWageBuilder.py
@@ -0,0 +1,9 @@
+'''
+@Author: Naziya
+@Date: 2021-08-08
+@Last Modified by: Naziya
+@Last Modified Time: 2021-08-08 09:15:00
+@Title: Welcome message
+
+'''
+print("Welcome to Employee Wage Computation Problem")
