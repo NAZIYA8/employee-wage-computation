ac898dea06e0938d3cfb46b4cec3c5f5fea03d68
NAZIYA8 <syeda.naziya896@gmail.com>
2021-07-08 23:58:31 +0530

    [Naziya]Add: Parttime employee added and wage is calculated

--- employee_wage_builder.py
+++ employee_wage_builder.py
@@ -2,13 +2,15 @@
 @Author: Naziya
 @Date: 2021-08-08
 @Last Modified by: Naziya
-@Last Modified Time: 2021-08-08 09:15:00
+@Last Modified Time: 2021-08-08 23:51:00
 @Title: Aim of the program is to calculate the Employee Daily and Monthly Wage.
 '''
 
 import random
 IS_FULL_TIME = 1
+IS_PART_TIME = 2
 EMP_RATE_PER_HOUR = 20
+PART_TIME_RATE_PER_HOUR = 8
 
 
 class EmployeeWageBuilder:
@@ -19,6 +21,9 @@ class EmployeeWageBuilder:
     if check_attendence == IS_FULL_TIME:
         emp_hrs = 8
     else:
-        emp_hrs = 0
+        emp_hrs = 4
     emp_wage = emp_hrs * EMP_RATE_PER_HOUR
-    print("Daily Employee Wage is: ", emp_wage)
+    print("Daily Employee Wage is:", emp_wage)
+    part_time_hrs = random.randint(1, 8)
+    part_time_wage = part_time_hrs * PART_TIME_RATE_PER_HOUR
+    print("Part Time Employee Wages:", part_time_wage)
