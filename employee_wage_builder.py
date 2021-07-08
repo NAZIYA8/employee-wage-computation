bee7f0f0ea44144eeb04f4c42f44077f6afbb9e8
NAZIYA8 <syeda.naziya896@gmail.com>
2021-07-09 00:39:07 +0530

    [Naziya]Add: Parttime employee added and wage is calculated using Switch case

--- employee_wage_builder.py
+++ employee_wage_builder.py
@@ -2,11 +2,12 @@
 @Author: Naziya
 @Date: 2021-08-08
 @Last Modified by: Naziya
-@Last Modified Time: 2021-08-08 23:51:00
+@Last Modified Time: 2021-08-08 0:20:00
 @Title: Aim of the program is to calculate the Employee Daily and Monthly Wage.
 '''
 
 import random
+from typing import DefaultDict
 IS_FULL_TIME = 1
 IS_PART_TIME = 2
 EMP_RATE_PER_HOUR = 20
@@ -14,16 +15,15 @@ PART_TIME_RATE_PER_HOUR = 8
 
 
 class EmployeeWageBuilder:
-    def __init__(self):
-        self.check_attendence = 0
+    def __init__(self, check_attendence=0, emp_hrs=0, emp_wage=0):
+        self.check_attendence = check_attendence
 
-    check_attendence = random.randint(0, 1)
-    if check_attendence == IS_FULL_TIME:
+    switch = random.randint(1, 3)
+    if switch == 1:
         emp_hrs = 8
-    else:
+    elif switch == 2:
         emp_hrs = 4
+    else:
+        emp_hrs = 0
     emp_wage = emp_hrs * EMP_RATE_PER_HOUR
-    print("Daily Employee Wage is:", emp_wage)
-    part_time_hrs = random.randint(1, 8)
-    part_time_wage = part_time_hrs * PART_TIME_RATE_PER_HOUR
-    print("Part Time Employee Wages:", part_time_wage)
+    print("Employee Wage is:", emp_wage)
