fd9be867dee7aea8ded7e1e272c8b1ab358ef507
NAZIYA8 <syeda.naziya896@gmail.com>
2021-07-08 23:47:44 +0530

    [Naziya]Add: Calculated the daily employee wage of employee

--- employee_wage_builder.py
+++ employee_wage_builder.py
@@ -7,13 +7,18 @@
 '''
 
 import random
+IS_FULL_TIME = 1
+EMP_RATE_PER_HOUR = 20
+
 
 class EmployeeWageBuilder:
     def __init__(self):
         self.check_attendence = 0
 
     check_attendence = random.randint(0, 1)
-    if check_attendence == 1:
-        print('Employee is present')
+    if check_attendence == IS_FULL_TIME:
+        emp_hrs = 8
     else:
-        print('Employee is absent')
+        emp_hrs = 0
+    emp_wage = emp_hrs * EMP_RATE_PER_HOUR
+    print("Daily Employee Wage is: ", emp_wage)
